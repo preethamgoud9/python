a3651f21e8c1db3b1133b876cf38cbf844bfb7ce
preethamgoud9 <preethamgoud229@gmail.com>
2026-03-13 19:54:33 +0530

    #dsa-19

---
+++ string_palindrome.py
@@ -0,0 +1,13 @@
+s = "markram"
+
+left = 0
+right = len(s) - 1
+
+while left < right:
+    if s[left] != s[right]:
+        print("not an palindrome")
+        break
+    left += 1
+    right -=1
+else:
+    print("it is an palindrome")
