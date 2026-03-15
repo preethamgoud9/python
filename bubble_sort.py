dbdeea6660b2b959b8224a589d8604085b719306
preethamgoud9 <preethamgoud229@gmail.com>
2026-03-15 23:54:41 +0530

    #dsa-24

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,12 @@
 nums = [5,8,1,6,9,2,4]
 
 for i in range(len(nums)):
+    is_swap = False
     for j in range(0,len(nums)- i - 1 ):
         if nums[j] > nums[j+1]:
             nums[j],nums[j+1] = nums[j+1],nums[j]
+            is_swap = True
+    if is_swap == False:
+        break
 
 print(nums)
--- bubble_sort_desc.py
+++ bubble_sort_desc.py
@@ -4,4 +4,5 @@ for i in range(len(nums)):
     for j in range(0,len(nums) - i - 1):
         if nums[j] < nums[j+1]:
             nums[j],nums[j+1] = nums[j+1],nums[j]
+            
 print(nums)
