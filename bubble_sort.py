8206825e7b8843760a6a63b924748e37cfbc5d35
preethamgoud9 <preethamgoud229@gmail.com>
2026-03-17 21:44:06 +0530

    #dsa-25

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@ nums = [5,8,1,6,9,2,4]
 
 for i in range(len(nums)):
     is_swap = False
-    for j in range(0,len(nums)- i - 1 ):
+    for j in range(len(nums)-i-1):
         if nums[j] > nums[j+1]:
             nums[j],nums[j+1] = nums[j+1],nums[j]
             is_swap = True
